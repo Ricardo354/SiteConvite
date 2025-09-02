27999a219eb5b4e9bac39ed9c44ddb701992f1d2
Ricardo354 <yellow_richard@proton.me>
2025-09-01 21:32:23 -0300

    Adding favico, fixing responsive behavior

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import person from './assets/juliana.png'
 
 function App() {
 
   return (
     <>
+      <title>Jully Fest 2025</title>
       <div className="flex justify-between">
 
 
@@ -18,18 +18,22 @@ function App() {
         <div className=" w-[1%] bg-[#3d0707]"></div>
 
         {/* Hero */}
-        <div className="flex flex-col flex-1 bg-white border-20  border-[#3d0707] ">
+        <div className="flex flex-col flex-1 bg-white border-20 border-[#3d0707] ">
           <div className="p-[2%]">
-            <p className="text-8xl text-[#6d0605] font-bold font-anton leading-[115px]">
-              <span>Jully Fest</span>
-              <span className="block lg:inline"> 2025</span>
-            </p>
-            <p className="text-4xl text-[#6d0605] font-poppins font-light">15 ANOS</p>
+            <div className="gap-2">
+              <p className="text-7xl leading-[90px] sm:text-8xl text-[#6d0605] font-bold font-anton sm:leading-[115px]">
+                <span>Jully Fest</span>
+                <span className=""> 2025</span>
+                <p className="inline  ml-[15px] -mt-[1px] lg:m-0 lg:block text-4xl text-[#6d0605] font-poppins font-light">15 ANOS</p>
+              </p>
+         
+            </div>
           </div>
+
 
           <div className="flex flex-col lg:flex-row -mt-[4%] p-[5%] lg:p-[4%] h-auto ">
 
-            <div className='bg-[url(./assets/bg.jpeg)] h-[500px] w-[100%] lg:h-auto bg-cover lg:w-[55%] '></div>
+            <div className='bg-[url(./assets/bg.jpeg)] bg-center h-[500px] w-[100%] lg:h-auto bg-cover lg:w-[55%] '></div>
 
             <div className='flex flex-col mt-[8%] lg:w-[45%] lg:mt-0 ml-[2%] gap-[30px]'>
               <p className='leading-[55px] text-5xl font-bebas text-[#3d0707]'>Separe seu melhor papel cinematográfico e venha participar da maior premiação da América:</p>
@@ -44,29 +48,29 @@ function App() {
             <div class="mx-4 w-4 h-4 bg-black rotate-45"></div>
             <div class="flex-1 h-px bg-black"></div>
           </div>
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 bg-[#3d0707] p-[2%]'>
+          <div className='grid grid-cols-2  lg:grid-cols-4 gap-4 bg-[#3d0707] p-[2%]'>
             <div className='text-white'>
               <p className='font-alice text-3xl'>Local:</p>
-              <a href="https://maps.app.goo.gl/qZjZrz9zsiVT7ugo6?g_st=aw" target='_blank' className='underline font-bebas text-5xl'>
+              <a href="https://maps.app.goo.gl/qZjZrz9zsiVT7ugo6?g_st=aw" target='_blank' className='underline font-bebas text-4xl sm:text-5xl'>
                 ESPAÇO FLOR <br /> NUNES
               </a>
             </div>
 
             <div className='text-white'>
               <p className='font-alice text-3xl'>Data:</p>
-              <p className='font-bebas text-5xl'>01.11.2025</p>
+              <p className='font-bebas text-4xl sm:text-5xl'>01.11.2025</p>
             </div>
 
             <div className='text-white'>
               <p className='font-alice text-3xl'>Hora:</p>
-              <p className='font-bebas text-5xl'>19:00 - <br />CREPÚSCULO</p>
+              <p className='font-bebas text-4xl sm:text-5xl'>19:00 - <br />CREPÚSCULO</p>
             </div>
 
             <div className='text-white'>
               <a href='https://forms.gle/B6jAaGv1zE1iYAuY6' target='_blank' className='font-alice text-3xl'>
                 Clique para realizar a<br />
               </a>
-              <a href='https://forms.gle/B6jAaGv1zE1iYAuY6' target='_blank' className='font-bebas text-5xl'>
+              <a href='https://forms.gle/B6jAaGv1zE1iYAuY6' target='_blank' className='font-bebas text-4xl sm:text-5xl'>
                 CONFIRMAÇÃO
               </a>
             </div>
